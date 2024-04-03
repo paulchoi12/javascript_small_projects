71780230d7f7303bdb20f3f6c99784f6d4c52626
Melmow <paulchoi12@naver.com>
2024-04-03 15:46:14 +0100

    component ScrollToTop added

--- MiniWebsite/mini-website/src/App.js
+++ MiniWebsite/mini-website/src/App.js
@@ -7,15 +7,17 @@ import About from './components/Pages/About.js';
 import './App.css'
 import Cambridge from './components/Pages/Cambridge/Cambridge.js';
 import Chester from './components/Pages/Chester.js';
+import ScrollToTop from './components/ScrollToTop.js';
 function App() {
   return (
     <Router>
+      <ScrollToTop/>
       <Navbar/>
       <Routes>
         <Route path='/' exact Component={Home}/>
         <Route path='/About' exact Component={About}/>
-        <Route path='/Cambridge' exact Component={Cambridge}></Route>
-        <Route path='/Chester' exact Component={Chester}></Route>
+        <Route path='/Cambridge' exact Component={Cambridge}/>
+        <Route path='/Chester' exact Component={Chester}/>
       </Routes>
     </Router>
   );
