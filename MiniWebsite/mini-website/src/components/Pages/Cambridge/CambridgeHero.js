a0c1390b542b79da37d5ed9563e9717f18462eeb
Melmow <paulchoi12@naver.com>
2024-04-12 20:58:11 +0100

    added cambridge appearmotion

--- MiniWebsite/mini-website/src/components/Pages/Cambridge/CambridgeHero.js
+++ MiniWebsite/mini-website/src/components/Pages/Cambridge/CambridgeHero.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import '../../HeroSection.css'
+import {motion} from 'framer-motion'
+import { routeVariants, childVariants } from '../../Variants'
 function CambridgeHero() {
   return (
-    <div className='hero-container'>
+    <motion.div 
+    variants={routeVariants}
+      initial="initial"
+      animate="final"
+    className='hero-container'>
       <img className='hero-image' src='/images/JesusGreen.jpeg' alt='lamp'/>
-        <h1>Cambridge</h1>
-        <p>Places I've been to</p>
-    </div>
+        <motion.h1 variants={childVariants} initial="initial" animate="final">Cambridge</motion.h1>
+        <motion.p variants={childVariants} initial="initial" animate="final">Places I've been to</motion.p>
+    </motion.div>
   )
 }
 
