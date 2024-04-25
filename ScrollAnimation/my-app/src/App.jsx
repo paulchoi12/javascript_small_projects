322fa07e1f8c9ee56c0c15d44ab08a655124864b
Melmow <paulchoi12@naver.com>
2024-04-25 18:18:04 +0100

    scrolling animation working

--- ScrollAnimation/my-app/src/App.jsx
+++ ScrollAnimation/my-app/src/App.jsx
@@ -1,29 +1,42 @@
-import { useState } from 'react'
 
 import { motion, useAnimation } from "framer-motion";
+
 
 import { useInView } from "react-intersection-observer";
 
 import { useEffect } from "react";
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const boxVariant = {
-    visible: { opacity: 1, scale: 2, transition: {duration: 1.5}},
+
+   // Animation Variant
+   const boxVariant = {
+    visible: { opacity: 1, scale: 2, transition: {duration: 0.5}},
     hidden: { opacity: 0, scale: 0 },
   }
 
   /*Box component*/
 const Box = () => {
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
   return (
     <motion.div 
-    className='box'
-    variants={boxVariant}
-    initial='hidden'
-    animate='visible'
+      className="box"
+      ref={ref}
+      variants={boxVariant}
+      initial="hidden"
+      animate={control}
     >
       <h1>Box</h1>
     </motion.div>
@@ -32,8 +45,16 @@ const Box = () => {
 
   return (
   <div className="App">
-    <Box/>
-    <Box/>
+    <div className="boxes">
+      <ul clasName="boxList">
+        <li><Box/></li>
+        <li><Box/></li>
+        <li><Box/></li>
+        <li><Box/></li>
+        
+      </ul>
+    </div>
+   
   </div>
   )
 }
